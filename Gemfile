bd5d82eed33de9e9335ed99dd8581b61c5e25760
ayiskane <159972388+ayiskane@users.noreply.github.com>
2024-10-01 01:59:09 +0000

    added bootstrap, popper, FA cdns

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ gem "omniauth"
 gem "omniauth-discord"
 gem "omniauth-rails_csrf_protection"
 
+gem "hotwire-livereload", "~> 1.4", group: :development
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "controllers";
+import * as bootstrap from "bootstrap";
+import { createPopper } from "@popperjs/core";
+import { Turbo } from "@hotwired/turbo-rails";
+Turbo.start();
+
+document.addEventListener("DOMContentLoaded", function () {
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
