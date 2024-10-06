ebf9f6614765a95175140953c62be552d14b549d
ptdo <dpt.dothao98@gmail.com>
2024-10-06 07:14:14 +0000

    delete unused migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_24_230732) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_26_210132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +20,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_230732) do
     t.string "name_cn2", null: false
     t.string "name_jp", null: false
     t.string "name_kr", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["name_cn"], name: "index_areas_on_name_cn"
     t.index ["name_cn2"], name: "index_areas_on_name_cn2"
     t.index ["name_en"], name: "index_areas_on_name_en"
@@ -29,137 +27,53 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_230732) do
     t.index ["name_kr"], name: "index_areas_on_name_kr"
   end
 
+  create_table "asset_keys", id: false, force: :cascade do |t|
+    t.string "id", null: false
+    t.string "img1", null: false
+    t.string "img2"
+    t.string "img3"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["id"], name: "index_asset_keys_on_id"
+    t.index ["img1"], name: "index_asset_keys_on_img1"
+    t.index ["img2"], name: "index_asset_keys_on_img2"
+    t.index ["img3"], name: "index_asset_keys_on_img3"
+  end
+
+  create_table "assets", id: false, force: :cascade do |t|
+    t.string "id", null: false
+    t.string "uuid", null: false
+    t.string "url", null: false
+    t.decimal "version", precision: 5, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["id", "url"], name: "index_assets_on_id_and_url", unique: true
+    t.index ["url"], name: "index_assets_on_url"
+    t.index ["version"], name: "index_assets_on_version"
+  end
+
   create_table "currencies", id: :string, force: :cascade do |t|
-    t.string "name_en", null: false
-    t.string "name_cn", null: false
-    t.string "name_cn2", null: false
-    t.string "name_jp", null: false
-    t.string "name_kr", null: false
-    t.string "icon"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["icon"], name: "index_currencies_on_icon"
-    t.index ["name_cn"], name: "index_currencies_on_name_cn"
-    t.index ["name_cn2"], name: "index_currencies_on_name_cn2"
-    t.index ["name_en"], name: "index_currencies_on_name_en"
-    t.index ["name_jp"], name: "index_currencies_on_name_jp"
-    t.index ["name_kr"], name: "index_currencies_on_name_kr"
+    t.string "name"
+    t.integer "costtype"
+    t.index ["costtype"], name: "index_currencies_on_costtype"
+    t.index ["id"], name: "index_currencies_on_id"
+    t.index ["name"], name: "index_currencies_on_name"
   end
 
-  create_table "events", id: :string, force: :cascade do |t|
-    t.datetime "start_date"
-    t.datetime "end_date"
+  create_table "localize_data", id: :string, force: :cascade do |t|
+    t.string "en"
+    t.string "cn"
+    t.string "cn2"
+    t.string "jp"
+    t.string "kr"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "food_types", id: :string, force: :cascade do |t|
-    t.string "area_id", null: false
-    t.string "name_en", null: false
-    t.string "name_cn", null: false
-    t.string "name_cn2", null: false
-    t.string "name_jp", null: false
-    t.string "name_kr", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["area_id"], name: "index_food_types_on_area_id"
-    t.index ["name_cn"], name: "index_food_types_on_name_cn"
-    t.index ["name_cn2"], name: "index_food_types_on_name_cn2"
-    t.index ["name_en"], name: "index_food_types_on_name_en"
-    t.index ["name_jp"], name: "index_food_types_on_name_jp"
-    t.index ["name_kr"], name: "index_food_types_on_name_kr"
-  end
-
-  create_table "mail_categories", id: :string, force: :cascade do |t|
-    t.string "name_en", null: false
-    t.string "name_cn", null: false
-    t.string "name_cn2", null: false
-    t.string "name_jp", null: false
-    t.string "name_kr", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name_cn"], name: "index_mail_categories_on_name_cn"
-    t.index ["name_cn2"], name: "index_mail_categories_on_name_cn2"
-    t.index ["name_en"], name: "index_mail_categories_on_name_en"
-    t.index ["name_jp"], name: "index_mail_categories_on_name_jp"
-    t.index ["name_kr"], name: "index_mail_categories_on_name_kr"
-  end
-
-  create_table "mail_groups", id: :string, force: :cascade do |t|
-    t.string "name_en", null: false
-    t.string "name_cn", null: false
-    t.string "name_cn2", null: false
-    t.string "name_jp", null: false
-    t.string "name_kr", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name_cn"], name: "index_mail_groups_on_name_cn"
-    t.index ["name_cn2"], name: "index_mail_groups_on_name_cn2"
-    t.index ["name_en"], name: "index_mail_groups_on_name_en"
-    t.index ["name_jp"], name: "index_mail_groups_on_name_jp"
-    t.index ["name_kr"], name: "index_mail_groups_on_name_kr"
-  end
-
-  create_table "staff_levels", id: false, force: :cascade do |t|
-    t.string "staff_id", null: false
-    t.integer "level", null: false
-    t.integer "need_coin", null: false
-    t.integer "gain_star", null: false
-    t.time "need_time", null: false
-    t.time "work_time"
-    t.time "rest_time"
-    t.integer "speed"
-    t.decimal "promo_time", precision: 3, scale: 1
-    t.integer "gain_customer"
-    t.time "sweep_time"
-    t.integer "gain_tips"
-    t.integer "cook_speed"
-    t.time "cook_time"
-    t.integer "gain_coin_per_minute"
-    t.integer "speed_boost"
-    t.time "frequency"
-    t.decimal "driveout_time", precision: 3, scale: 1
-    t.time "min_return_time"
-    t.time "max_return_time"
-    t.integer "grow_speed"
-    t.time "grow_time"
-    t.integer "gain_plate"
-    t.integer "catch_chance"
-    t.integer "gain_bait"
-    t.integer "delivery_order_limit"
-    t.integer "delivery_efficiency"
-    t.integer "gain_order_limit"
-    t.integer "gain_like_chance"
-    t.integer "gain_like"
-    t.integer "min_drop"
-    t.integer "max_drop"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["staff_id"], name: "index_staff_levels_on_staff_id"
-  end
-
-  create_table "staffs", id: :string, force: :cascade do |t|
-    t.string "area_id", null: false
-    t.string "name_en", null: false
-    t.string "desc_en"
-    t.string "name_cn", null: false
-    t.string "desc_cn"
-    t.string "name_cn2", null: false
-    t.string "desc_cn2"
-    t.string "name_jp", null: false
-    t.string "desc_jp"
-    t.string "name_kr", null: false
-    t.string "desc_kr"
-    t.string "icon"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["area_id"], name: "index_staffs_on_area_id"
-    t.index ["icon"], name: "index_staffs_on_icon"
-    t.index ["name_cn"], name: "index_staffs_on_name_cn"
-    t.index ["name_cn2"], name: "index_staffs_on_name_cn2"
-    t.index ["name_en"], name: "index_staffs_on_name_en"
-    t.index ["name_jp"], name: "index_staffs_on_name_jp"
-    t.index ["name_kr"], name: "index_staffs_on_name_kr"
+    t.index ["cn"], name: "index_localize_data_on_cn"
+    t.index ["cn2"], name: "index_localize_data_on_cn2"
+    t.index ["en"], name: "index_localize_data_on_en"
+    t.index ["id"], name: "index_localize_data_on_id", unique: true
+    t.index ["jp"], name: "index_localize_data_on_jp"
+    t.index ["kr"], name: "index_localize_data_on_kr"
   end
 
   create_table "users", force: :cascade do |t|
@@ -175,8 +89,4 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_24_230732) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
   end
-
-  add_foreign_key "food_types", "areas"
-  add_foreign_key "staff_levels", "staffs"
-  add_foreign_key "staffs", "areas"
 end
